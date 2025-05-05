54008cc37bf9906c4b79e8bb0ce59fea0bcc21e8
dit0mad <yasantha2019@gmail.com>
2025-05-05 15:48:33 -0400

    add localization support

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,18 @@
+import 'app_localizations.dart';
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([super.locale = 'en']);
+
+  @override
+  String get helloUser => 'Hello User';
+
+  @override
+  String get selectLanguage => 'Select Language';
+
+  @override
+  String get english => 'English';
+
+  @override
+  String get spanish => 'Spanish';
+}
---
+++ lib/l10n/app_localizations_es.dart
@@ -0,0 +1,18 @@
+import 'app_localizations.dart';
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([super.locale = 'es']);
+
+  @override
+  String get helloUser => 'Hola Usuario';
+
+  @override
+  String get selectLanguage => 'Seleccionar idioma';
+
+  @override
+  String get english => 'Inglés';
+
+  @override
+  String get spanish => 'Español';
+}
