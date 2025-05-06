e0826a480009d7918627d5af5236f9d12ffe8898
dit0mad <yasantha2019@gmail.com>
2025-05-05 20:08:52 -0400

    We know that the language selection will only show when state is ready. so remove all subscriptions to language bloc and listen to bloc's value.

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -5,6 +5,8 @@ import 'package:localization_demo/blocs/l10n_bloc/bloc.dart';
 import 'package:localization_demo/blocs/l10n_bloc/models/language_model.dart';
 import 'package:localization_demo/l10n/app_localizations.dart';
 
+
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -23,28 +25,25 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final currentLanguage = context.watch<LanguageBloc>().state.currentLanguage;
+    final locale = AppLocalizations.of(context)!;
+
+    final currentLanguage = bloc.state.currentLanguage;
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(AppLocalizations.of(context)!.appTitle),
+        title: Text(locale.appTitle),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              AppLocalizations.of(context)!.helloUser,
+              locale.helloUser,
               style: const TextStyle(fontSize: 24),
             ),
             const SizedBox(height: 20),
             DropdownButton<LanguageModel>(
-              icon: const Icon(Icons.language),
-              isDense: true,
               value: currentLanguage,
-              alignment: Alignment.centerLeft,
-              isExpanded: false,
-              menuMaxHeight: 300,
               onChanged: (LanguageModel? newLanguage) {
                 if (newLanguage != null) {
                   bloc.add(ChangeLanguageAction(language: newLanguage));
